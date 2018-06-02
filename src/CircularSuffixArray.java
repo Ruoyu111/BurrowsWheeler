63bb2c220cbb5eb2b3e06b21a569b17a9ce6e0ab
Ruoyu Chen <chen.ruo@husky.neu.edu>
2018-06-02 16:55:12 -0400

    Added test files, and CircularSuffixArray java

---
+++ src/CircularSuffixArray.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class CircularSuffixArray {
+
+    private int[] index;
+    private int length;
+    private IndexString[] suffixes;
+
+    private class IndexString implements Comparable<IndexString> {
+        String str;
+        int index;
+
+        @Override
+        public int compareTo(IndexString o) {
+            return this.str.compareTo(o.str);
+        }
+    }
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+        if (s == null)
+            throw new IllegalArgumentException("argument is null");
+        this.length = s.length();
+        this.index = new int[this.length];
+        this.suffixes = new IndexString[this.length];
+
+        char[] chars = (s + s).toCharArray();
+        for (int i = 0; i < this.length; i++) {
+            IndexString is = new IndexString();
+            is.str = new String(chars, i, this.length);
+            is.index = i;
+            suffixes[i] = is;
+        }
+
+        Arrays.sort(suffixes);
+
+        for (int c = 0; c < this.length; c++) {
+            index[c] = suffixes[c].index;
+        }
+    }
+
+    // length of s
+    public int length() {
+        return length;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        if (i < 0 || i >= length)
+            throw new IllegalArgumentException("input is outside of its prescribed range.");
+        return index[i];
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        String s = "ABRACADABRA!";
+        CircularSuffixArray csa = new CircularSuffixArray(s);
+        System.out.println("Length: " + csa.length());
+        for (int i = 0; i < csa.length(); i++) {
+            System.out.println(csa.index(i));
+        }
+    }
+}
--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -4,59 +4,61 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class MoveToFront {
-	
-	// apply move-to-front encoding, reading from standard input and writing to standard output
-	public static void encode() {
-		
-		LinkedList<Character> chars = new LinkedList<>();
-		for (int i = 0; i < 256; i++) {
-			chars.add((char) i);
-		}
-		
-		while (!BinaryStdIn.isEmpty()) {
-			// read char
-			char c = BinaryStdIn.readChar();
-			BinaryStdOut.write((char) chars.indexOf(c));
-			
-			// move to front
-			if (chars.indexOf(c) != 0) {
-				chars.remove(new Character(c));
-				chars.addFirst(c);
-			}
-		}
-		BinaryStdOut.flush();
-	}
-	
-	// apply move-to-front decoding, reading from standard input and writing to standard output
-	public static void decode() {
-		LinkedList<Character> chars = new LinkedList<>();
-		for (int i = 0; i < 256; i++) {
-			chars.add((char) i);
-		}
-		
-		while (!BinaryStdIn.isEmpty()) {
-			// read integer
-			int index = BinaryStdIn.readChar();
-			char c = chars.get(index);
-			BinaryStdOut.write(c);
-			
-			// move to front
-			if (index != 0) {
-				chars.remove(index);
-				chars.addFirst(c);
-			}
-		}
-		BinaryStdOut.flush();
-	}
 
-	// if args[0] is '-', apply move-to-front encoding
-	// if args[0] is '+', apply move-to-front decoding
-	public static void main(String[] args) {
-		if (args[0].equals("-")) {
-			encode();
-		} else if (args[0].equals("+")) {
-			decode();
-		}
-	}
+    // apply move-to-front encoding, reading from standard input and writing to
+    // standard output
+    public static void encode() {
+
+        LinkedList<Character> chars = new LinkedList<>();
+        for (int i = 0; i < 256; i++) {
+            chars.add((char) i);
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+            // read char
+            char c = BinaryStdIn.readChar();
+            BinaryStdOut.write((char) chars.indexOf(c));
+
+            // move to front
+            if (chars.indexOf(c) != 0) {
+                chars.remove(new Character(c));
+                chars.addFirst(c);
+            }
+        }
+        BinaryStdOut.flush();
+    }
+
+    // apply move-to-front decoding, reading from standard input and writing to
+    // standard output
+    public static void decode() {
+        LinkedList<Character> chars = new LinkedList<>();
+        for (int i = 0; i < 256; i++) {
+            chars.add((char) i);
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+            // read integer
+            int index = BinaryStdIn.readChar();
+            char c = chars.get(index);
+            BinaryStdOut.write(c);
+
+            // move to front
+            if (index != 0) {
+                chars.remove(index);
+                chars.addFirst(c);
+            }
+        }
+        BinaryStdOut.flush();
+    }
+
+    // if args[0] is '-', apply move-to-front encoding
+    // if args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args) {
+        if (args[0].equals("-")) {
+            encode();
+        } else if (args[0].equals("+")) {
+            decode();
+        }
+    }
 
 }
