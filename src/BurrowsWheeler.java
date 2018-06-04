fc35fb95a88fe8a9b118555cbf8619ad0e9ebfe6
Ruoyu Chen <chen.ruo@husky.neu.edu>
2018-06-04 19:47:47 -0400

    added inverseTransform

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
@@ -32,7 +37,49 @@ public class BurrowsWheeler {
 	
 	// apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
 	public static void inverseTransform() {
+		int first = BinaryStdIn.readInt();
+		// tail of Sorted Suffixes
+		List<Character> t = new ArrayList<>();
+		// head of Sorted Suffixes
+		List<Character> h = new ArrayList<>();
+		while (!BinaryStdIn.isEmpty()) {
+		    char c = BinaryStdIn.readChar();
+		    t.add(c);
+		    h.add(c);
+		}
+		Collections.sort(h);
 		
+		int len = t.size();
+		
+		// origin string
+		char[] origin = new char[len];
+		origin[0] = h.get(first);
+		origin[len - 1] = t.get(first);
+		
+		// build next array
+		int[] next = new int[len];
+		HashSet<Integer> set = new HashSet<>();
+		
+		for (int i = 0; i < len; i++) {
+		    for (int j = 0; j < len; j++) {
+		        if (i == j) continue;
+		        if (h.get(i).equals(t.get(j)) && !set.contains(j)) {
+		            next[i] = j;
+		            set.add(j);
+		        }
+		    }
+		}
+		
+		// build origin string
+		for (int i = 1, nextIndex = next[first]; i < len - 1; i++, nextIndex = next[nextIndex]) {
+		    origin[i] = h.get(nextIndex);
+		}
+		
+		// output
+		for (int i = 0; i < len; i++) {
+		    BinaryStdOut.write(origin[i]);
+		}
+		BinaryStdOut.close();
 	}
 
 	// if args[0] is '-', apply Burrows-Wheeler transform
@@ -40,6 +87,8 @@ public class BurrowsWheeler {
 	public static void main(String[] args) {
 		if (args[0].equals("-")) {
 			transform();
+		} else if (args[0].equals("+")) {
+		    inverseTransform();
 		}
 	}
 }
